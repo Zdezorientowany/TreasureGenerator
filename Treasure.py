c3e4fb84d9daf4254d563f2f907c4a3990789a1d
Sebastian Koza <kozasebastian45@gmail.com>
2023-04-02 00:48:25 +0200

    Changed all UI

--- Treasure.py
+++ Treasure.py
@@ -1,3 +1,4 @@
+from colorama import Fore, Style
 class Treasure:
     
     def __init__(self, _name, _value, _size, _material, _quality):
@@ -8,5 +9,37 @@ class Treasure:
         self.value = int(_value * (2 if self.quality == "Flawless" else 1 if self.quality == "Good Quality" else 0.5) * (4 if self.material == "Jeweled" else 3 if self.material == "Gold" else 2 if self.material == "Silver" else 1))
     
     def __str__(self):
-        return f"""{self.material} {self.quality} {self.name} - {self.value}g - {self.size}"""
-
+        # Format the quality and material
+        quality_str = ""
+        if self.quality == "Flawless":
+            quality_str = Fore.MAGENTA + self.quality + Style.RESET_ALL
+        elif self.quality == "Good Quality":
+            quality_str = Fore.YELLOW + self.quality + Style.RESET_ALL
+        else:
+            quality_str = Fore.RED + self.quality + Style.RESET_ALL
+        
+        material_str = ""
+        if self.material == "Jeweled":
+            material_str = Fore.RED + self.material + Style.RESET_ALL
+        elif self.material == "Gold":
+            material_str = Fore.YELLOW + self.material + Style.RESET_ALL
+        elif self.material == "Silver":
+            material_str = Fore.CYAN + self.material + Style.RESET_ALL
+        else:
+            material_str = Fore.WHITE + self.material + Style.RESET_ALL
+        
+        # Format the value
+        value_str = Fore.YELLOW + str(self.value) + "g" + Style.RESET_ALL
+        
+        # Format the size
+        size_str = self.size.capitalize()
+        
+        # Combine all the formatted strings
+        item_str = "{:<25} {:<35} {:<15} {:<10}".format(
+            "\033[31m{}\033[0m {}".format(quality_str.capitalize(), material_str.capitalize()),
+            "\033[36m{}\033[0m".format(self.name),
+            "\033[33m{}\033[0m".format(value_str),
+            size_str
+        )
+        
+        return item_str
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from Treasure import Treasure
 import random
 import json
+import os
+import colorama
 
 with open('treasures.json') as f:
     tresure_dict = json.load(f)
@@ -15,39 +17,40 @@ quality_rarity = [0.7, 0.25, 0.05]
 material = ["Copper", "Silver", "Gold", "Jeweled"]
 material_rarity = [0.65, 0.2, 0.1, 0.05]
 
-print("Welcome to Tresure Generator!")
-print("1. Generate treasures")#Advanced Generator with promtps
-print("2. Quick Generete")#Generate random tresures by one click
-print("3. Options")#You can change crucial variables needed for quick Generate
-print("4. Exit")
-
+colorama.init()
+os.system("cls || clear")
 while True:
+    
+    print("\n\033[93mWelcome to Treasure Generator!\033[0m")
+    print("1. Generate treasures")#Advanced Generator with promtps
+    print("2. Quick Generete")#Generate random tresures by one click
+    print("3. Options")#You can change crucial variables needed for quick Generate
+    print("4. Exit")
     choice = input("Enter your choice (1-4): ")
-
+    os.system("cls || clear")
     if choice == "1":
-        print("You chose Option 1.")
+        print("\033[93m=======Generate treasures=======\033[0m")
         # Add code to execute Option 1 here
     elif choice == "2":
-        print("\n=======Quick Generate=======")
+        print("\033[93m=======Quick Generate=======\033[0m")
         quantity = random.randint(0, MAX)
         result_quality = random.choices(quality,quality_rarity, k=quantity)
         result_material = random.choices(material,material_rarity, k=quantity)
         items = random.choices(tresure_dict["Items"], k=quantity)
-        print("You found",quantity,"items!")
-        print("============================")
+        print("\nYou found",quantity,"items!\n")
+        print("-" * 75)
         random_items = []
         for i in range(quantity):
             random_items.append(Treasure(items[i]["name"], items[i]["value"], items[i]["size"], result_material[i], result_quality[i]))
         for i in range(quantity):
             print(random_items[i])
-        print("============================")
-        print("Total value of items: "+str(sum(item.value for item  in random_items))+"g")
-        print("============================\n")
+        print("-" * 75)
+        print("\033[92m{:<67} {:<8}\033[0m".format('Total value of items:', str(sum(item.value for item in random_items)) + 'g'))
     elif choice == "3":
-        print("You chose Option 3.")
+        print("\033[93m=======Options=======\033[0m")
         # Add code to execute Option 3 here
     elif choice == "4":
-        print("Exiting the menu. Goodbye!")
+        print("\033[93mExiting the menu. Goodbye!\033[0m")
         break 
     else:
-        print("Invalid choice. Please enter a number between 1 and 4.")
+        print("\033[91mInvalid choice. Please enter a number between 1 and 4.\033[0m")
