e9659140844d5e887271c473411ab7de0c172b19
Sebastian Koza <kozasebastian45@gmail.com>
2023-04-01 22:25:46 +0200

    Tresure.py modified.

--- Tresure.py
+++ Tresure.py
@@ -1,6 +1,16 @@
-class Tresure:
+class Treasure:
     
-    def __init__(self, _name, _value, _size):
+    def __init__(self, _name, _value, _size, _material, _quality):
         self.name = _name
-        self.value = _value
         self.size = _size
+        self.material = _material
+        self.quality = _quality
+        self.value = _value * (2 if self.quality == "Flawless" else 1 if self.quality == "Good Quality" else 0.5) * (4 if self.material == "Jeweled" else 3 if self.material == "Gold" else 2 if self.material == "Silver" else 1)
+    
+    def __str__(self):
+        return f"""Name: {self.name},
+    Value: {self.value},
+    Size: {self.size},
+    Material: {self.material},
+    Quality: {self.quality}"""
+
