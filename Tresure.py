b146eb2eea41917c643ea34c49a88a619ae8ade7
Szymon Kilian <szymonkilian98@o2.pl>
2023-04-01 20:49:37 +0200

    added new variable(size) to class Tresure

--- Tresure.py
+++ Tresure.py
@@ -1,7 +1,6 @@
 class Tresure:
     
-    def __init__(self, _name, _value):
+    def __init__(self, _name, _value, _size):
         self.name = _name
         self.value = _value
-    
-     
+        self.size = _size
