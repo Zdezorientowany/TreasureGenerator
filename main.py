03db238b20153c490963d83d184e2f735f5435fa
Szymon Kilian <szymonkilian98@o2.pl>
2023-04-05 21:56:28 +0200

    Added presets.json with presets settings. Modified menu and added Presets Generate feature

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ with open('treasures.json') as f:
 with open('settings.json') as f:
     settings_dict = json.load(f)
 
+with open('presets.json') as f:
+    presets_dict = json.load(f)
+
 MAX = settings_dict["MAX"]
+MIN = settings_dict["MIN"]
 
 quality = list(settings_dict["quality"].keys())
 quality_rarity = list(settings_dict["quality"].values())
@@ -25,9 +29,10 @@ while True:
     print(format_text("\n"+"="*75,"main"))  
     print(format_text("-"*28,"dark"),format_text("Treasure Generator","light"),format_text("-"*27,"dark"))
     print("1. ",format_text("Advanced Generate","light"))#Advanced Generator with promtps
-    print("2. ",format_text("Quick Generate","light"))#Generate random tresures by one click
-    print("3. ",format_text("Options","light"))#You can change crucial variables needed for quick Generate
-    print("4. ",format_text("Exit","light"))
+    print("2. ",format_text("Quick Generate","light"))#Generate random treasures by one click
+    print("3. ",format_text("Preset Generate","light"))#Generate treasures from preconfigured settings
+    print("4. ",format_text("Options","light"))#You can change crucial variables needed for quick Generate
+    print("5. ",format_text("Exit","light"))
     print(format_text("-"*75,"dark")) 
     print(format_text("="*75,"main"))  
     choice = input("Enter your choice: ")
@@ -114,9 +119,7 @@ while True:
         print(format_text("\n"+"="*75,"main")) 
         print(format_text("-"*30,"dark"),format_text("Quick Generate","light"),format_text("-"*29,"dark"))
 
-        quantity = random.randint(0, MAX)
-        print(quality)
-        print(quality_rarity)
+        quantity = random.randint(MIN, MAX)
         result_quality = random.choices(quality,quality_rarity, k=quantity)
         result_material = random.choices(material,material_rarity, k=quantity)
         items = random.choices(tresure_dict["Items"], k=quantity)
@@ -133,9 +136,61 @@ while True:
         input()
         os.system("cls || clear")
 
-    elif choice in ("3", "O", "Options"):
+    elif choice in ("3", "P", "Preset", "Preset Generate", "Pre"):
+        flag = True
+        while flag:
+            print(format_text("\n"+"="*75,"main")) 
+            print(format_text("-"*30,"dark"),format_text("Preset Generate","light"),format_text("-"*29,"dark"))
+            for i in presets_dict:
+                print(i)
+            
+            print(format_text("\n"+"-"*75,"dark")) 
+            print(format_text("="*75,"main")) 
+            choice = input("Which preset you want to use?: ")
+            os.system("cls || clear")
+            print(format_text("\n"+"="*75,"main")) 
+            print(format_text("-"*30,"dark"),format_text(str(choice)+" Preset","light"),format_text("-"*29,"dark"))
+            chosen_preset = presets_dict[str(choice)]
+            quantity = random.randint(chosen_preset["MIN"], chosen_preset["MAX"])
+            while True: 
+                result_quality = random.choices(list(chosen_preset["quality"].keys()),list(chosen_preset["quality"].values()), k=quantity)
+                result_material = random.choices(list(chosen_preset["material"].keys()),list(chosen_preset["material"].values()), k=quantity)
+                items = random.choices(tresure_dict["Items"], k=quantity)
+                print(format_text("\nYou found "+str(quantity)+" items!","light"))
+                print(format_text("\n"+"="*75,"dark")) 
+                random_items = []
+                for i in range(quantity):
+                    random_items.append(Treasure(items[i]["name"], items[i]["value"], items[i]["size"], result_material[i], result_quality[i]))
+                print(tabulate([i.to_dict() for i in random_items], headers=[]))
+                print(format_text("="*75,"dark")) 
+                print('Total value of items:'+" "*48+ format_text(str(sum(item.value for item in random_items)) + 'g',"light"))
+                print(format_text("\n"+"-"*75,"dark")) 
+                print(format_text("="*75,"main"))              
+                print("1 - Roll again | 2 - Reroll treasures |  3 - Reset | 4 - Back to menu")
+                option = input("")
+                if option in ("1"):
+                    os.system("cls || clear")
+                    quantity = random.randint(chosen_preset["MIN"], chosen_preset["MAX"])
+                    continue
+                elif option in("2"):
+                    os.system("cls || clear")
+                    continue
+                elif option in("3"):
+                    os.system("cls || clear")
+                    break
+                elif option in("4"):
+                    os.system("cls || clear")
+                    flag = False
+                    break
+                else:
+                    os.system("cls || clear")
+                    print(format_text("Invalid choice. ","false"))
+                    continue
+
+    elif choice in ("4", "O", "Options"):
         print(format_text("="*75,"main")) 
         print(format_text("-"*33,"dark"),format_text("Options","light"),format_text("-"*33,"dark"))
+        print("Minimum items generated:",format_text(str(MIN),"light"))
         print("Maximum items generated:",format_text(str(MAX),"light"))
         print(format_text("-"*28,"dark"),format_text("Rarity of quality","light"),format_text("-"*28,"dark"))
         for x, y in zip(quality, quality_rarity):
@@ -145,10 +200,18 @@ while True:
             print(x+format_text(": {:.0%}".format(y),"light"))
         print(format_text("-"*75,"dark")) 
         print(format_text("="*75,"main")) 
-        print(format_text("Max(1) - Quality(2) - Material(3) - Exit(4)","main"))
+        print(format_text("Min(1) - Max(2) - Quality(3) - Material(4) - Exit(5)","main"))
         option = input(format_text("What parameter do you want to modify?: ","light"))
-
-        if option in("Max", "m", "1"):
+        if option in("Min", "1"):
+            try:
+                MAX = int(input(f"Enter new value for MIN({MIN}): "))
+                settings_dict["MIN"] = MIN
+                with open('settings.json', 'w') as f:
+                    json.dump(settings_dict, f, indent=4)
+            except ValueError:
+                os.system("cls || clear")
+                print(format_text("Invalid choice. Returning to menu ","false"))
+        elif option in("Max", "2"):
             try:
                 MAX = int(input(f"Enter new value for MAX({MAX}): "))
                 settings_dict["MAX"] = MAX
@@ -157,7 +220,7 @@ while True:
             except ValueError:
                 os.system("cls || clear")
                 print(format_text("Invalid choice. Returning to menu ","false"))
-        elif option in ("Quality", "Q", "2"):
+        elif option in ("Quality", "Q", "3"):
             try:
                 quality_choice = input(f"Enter a quality value ({', '.join([f'{q} ({r})' for q, r in zip(quality, quality_rarity)])}): ")
                 quality_index = quality.index(quality_choice)
@@ -169,7 +232,7 @@ while True:
             except ValueError:
                 os.system("cls || clear")
                 print(format_text("Invalid choice. Returning to menu ","false"))
-        elif option in ("Material", "M", "3"):
+        elif option in ("Material", "M", "4"):
             try:               
                 material_choice = input(f"Enter a material value ({', '.join([f'{m} ({r})' for m, r in zip(material, material_rarity)])}): ")
                 material_index = material.index(material_choice)
@@ -181,14 +244,14 @@ while True:
             except ValueError:
                 os.system("cls || clear")
                 print(format_text("Invalid input. Returning to menu ","false"))    
-        elif option in("Exit", "E", "4"):
+        elif option in("Exit", "E", "5"):
             print("Returning to menu...")  
         else:
             os.system("cls || clear")
             print(format_text("Invalid choice. Returning to menu ","false"))
         os.system("cls || clear")
 
-    elif choice in ("4", "e", "exit"):
+    elif choice in ("5", "e", "exit"):
         print(format_text("Exiting...","light")) 
         break 
     else:
