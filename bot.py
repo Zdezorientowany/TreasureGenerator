69948d02266320cb4f9969a979b26e89dfdaa60e
Sebastian Koza <kozasebastian45@gmail.com>
2023-04-06 01:14:41 +0200

    API key and ID moved to ini file

--- bot.py
+++ bot.py
@@ -1,6 +1,13 @@
 import discord
 import asyncio
 from main import generate_treasures
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+api_key = config.get('CONFIG', 'API_KEY')
+channel_id = config.get('CONFIG', 'CHANNEL_ID')
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -8,19 +15,13 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    channel = client.get_channel(1092570289843490897)
+    channel = client.get_channel(channel_id)
     await channel.send("```"+ generate_treasures(colors=False) + "```")
     await client.close()
 
 async def main():
-    await client.start('MTA5MjU2NTQ5NTQ2ODgwMjEzOQ.GqKIUK.dgPvt--rsqGpK-mPtuNmXZccVWdYXv2znI_h4A')
+    await client.start(api_key)
     print("Finished")
 
 print("\033[93m=======Sending the output to DISCORD=======\033[0m")
 asyncio.run(main())
-
-
-
-
-
-#MTA5MjU2NTQ5NTQ2ODgwMjEzOQ.GqKIUK.dgPvt--rsqGpK-mPtuNmXZccVWdYXv2znI_h4A
